7b38f7bf5d15a51e09e9c7b646528e13287f8edd
DatVT36 <vuthanhdat181@gmail.com>
2025-02-24 01:26:41 +0700

    feat: config gg maps [DatVT]

--- DemoGGMapsAPI/app/build.gradle.kts
+++ DemoGGMapsAPI/app/build.gradle.kts
@@ -32,12 +32,14 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.gms:play-services-maps:18.0.2")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
--- DemoGGMapsAPI/app/src/main/java/com/example/demoggmapsapi/MainActivity.java
+++ DemoGGMapsAPI/app/src/main/java/com/example/demoggmapsapi/MainActivity.java
@@ -3,13 +3,23 @@ package com.example.demoggmapsapi;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    SupportMapFragment mapFragment;
+    private GoogleMap myMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +30,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(googleMap -> {
+            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            googleMap.getUiSettings().setZoomControlsEnabled(true);
+        });
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        myMap = googleMap;
+
+        LatLng sydney = new LatLng(-34, 151);
+        myMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
